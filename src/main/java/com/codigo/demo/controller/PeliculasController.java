068d5012058a40d6d642d1c6ab702879a45ced06
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-12-02 01:50:01 -0300

    form de peliculas

--- src/main/java/com/codigo/demo/controller/PeliculasController.java
+++ src/main/java/com/codigo/demo/controller/PeliculasController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.codigo.demo.entities.Pelicula;
+import com.codigo.demo.service.IGeneroService;
 import com.codigo.demo.service.IPeliculaService;
 
 @Controller
@@ -14,6 +16,8 @@ public class PeliculasController {
 
   @Autowired
   private IPeliculaService service;
+  @Autowired
+  private IGeneroService genero;
 
   @GetMapping("pelicula")
   public String crear(Model model) {
@@ -27,8 +31,19 @@ public class PeliculasController {
   public String editar(@PathVariable() Long id, Model model) {
     Pelicula pelicula = new Pelicula();
     model.addAttribute("pelicula", pelicula);
+    model.addAttribute("generos", genero.findAll());
     model.addAttribute("titulo", "Nueva Pelicula");
     return "pelicula";
+  }
+
+  @PostMapping("/pelicula")
+  public String guardar(Pelicula pelicula) {
+    return "redirect:home";
+  }
+
+  @GetMapping({ "/", "/home", "/index" })
+  public String home() {
+    return "home";
   }
 
 }
